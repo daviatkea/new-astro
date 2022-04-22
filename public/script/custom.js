c1e0e434a28e285583bfefcbb3adf6d36f592d1a
Dannie Vinther <davi@kea.dk>
2022-04-22 13:28:22 +0200

    fix bugs

--- public/script/custom.js
+++ public/script/custom.js
@@ -1,14 +1,14 @@
 {
-  function observeHeader() {
-    const el = document.querySelector(".main-header");
-    if (!el) return;
-    const observer = new IntersectionObserver(
-      ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-      { threshold: 1 }
-    );
-    observer.observe(el);
-  }
-  observeHeader();
+  // function observeHeader() {
+  //   const el = document.querySelector(".main-header");
+  //   if (!el) return;
+  //   const observer = new IntersectionObserver(
+  //     ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  //     { threshold: 1 }
+  //   );
+  //   observer.observe(el);
+  // }
+  // observeHeader();
 
   const copyMe = document.querySelectorAll("h2 a[href]");
 
